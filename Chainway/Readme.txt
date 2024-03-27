88558d54fba4e49150c63198e219465a22d63710
aligatherin <alican@gatherin.life>
2024-03-28 02:25:12 +0300

    Chainway task

---
+++ Chainway/Readme.txt
@@ -0,0 +1,12 @@
+To run the project;
+Terminal opens
+* Go to the project's directory
+* run command -> node chainway.js
+* Postman application opens
+* A post request is created in postman.
+* In the postman post request, enter http://0.0.0.0:3001/new-tx in the url section.
+* In Postman, select the raw option in the body section and add the relevant txid; Orn: {
+    "txid": "11d53c83795205b512c92b142ce845e497b5724e3482498c3000ad29e2e23cc5"
+}
+
+Note: ChatGpt was used when creating the project.
---
+++ Chainway/chainway.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+
+const RPC_URL = "https://svc.blockdaemon.com/bitcoin/mainnet/native";
+const API_KEY = "zpka_a4f0aff81df04442a8facebbe46690e9_3796406a";
+const confirmationsThreshold = 6;
+const trackedTransactions = new Set();
+const finalizedTransactionsQueue = [];
+
+const authHeaders = {
+  headers: {
+    Authorization: `Bearer ${API_KEY}`,
+    "Content-Type": "application/json",
+  },
+};
+
+async function checkForFinalizedTransactions() {
+  for (const txid of trackedTransactions) {
+    try {
+      const response = await axios.post(
+        `${RPC_URL}/gettransaction`,
+        { txid },
+        authHeaders
+      );
+      const { confirmations, blockhash } = response.data;
+
+      if (confirmations >= confirmationsThreshold) {
+        finalizedTransactionsQueue.push(
+          `Transaction with txid ${txid} finalized at block with hash ${blockhash}`
+        );
+        trackedTransactions.delete(txid);
+      }
+    } catch (error) {
+      console.error(`Error checking transaction ${txid}: ${error.message}`);
+    }
+  }
+}
+
+setInterval(checkForFinalizedTransactions, 500);
+
+app.post("/new-tx", (req, res) => {
+  const { txid } = req.body;
+  if (!txid) {
+    return res.status(400).json({ error: "Missing 'txid' parameter" });
+  }
+
+  trackedTransactions.add(txid);
+
+  setTimeout(() => {
+    const finalized = finalizedTransactionsQueue.splice(
+      0,
+      finalizedTransactionsQueue.length
+    );
+    res.json(finalized);
+  }, 1000);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
